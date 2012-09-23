26bfa729c8bc81f2334f3ed6409e8d2a7cf0ba0f
antonepple <antonepple@192.168.178.25>
2012-09-23 13:36:03 +0200

    Added persistenceUtil

---
+++ World Editor/src/de/eppleton/jbox2d/persistence/PersitenceUtil.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.eppleton.jbox2d.persistence;
+
+/**
+ *
+ * @author antonepple
+ */
+public class PersitenceUtil {
+
+    public static org.jbox2d.dynamics.World getWorldFromJAXBWorld(World world) {
+        return null;
+    }
+
+    public static World getJAXBWorldFromWorld(org.jbox2d.dynamics.World world) {
+        return null;
+    }
+}
