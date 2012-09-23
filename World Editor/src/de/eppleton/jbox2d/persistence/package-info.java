8c2c353a79890b157965b00e383a54251aa2231d
antonepple <antonepple@192.168.178.25>
2012-09-23 16:58:54 +0200

    Added Schema based Code Completion

---
+++ World Editor/src/de/eppleton/jbox2d/persistence/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@TemplateRegistration(folder = "Box2D", content = "Box2DTemplate.b2x")
+package de.eppleton.jbox2d.persistence;
+
+import org.netbeans.api.templates.TemplateRegistration;
